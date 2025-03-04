241c5913df7ed277b02ded2c109791c6dfaaa857
Alexis Carreras <alexiscarreras@hotmail.com>
2025-03-04 00:11:42 -0300

    feat: add filtering and tag processing functions

---
+++ bff/src/utils/articlesUtils.ts
@@ -0,0 +1,24 @@
+import { Article, TopTag } from '../types/articleTypes';
+
+export const filterArticles = (articles: Article[]): Article[] => {
+  return articles.filter((article) => article.subtype === '7').slice(0, 30);
+};
+
+export const processTags = (articles: Article[]): TopTag[] => {
+  const tagCount: Record<string, { text: string; count: number }> = {};
+
+  articles.forEach((article) => {
+    article.taxonomy?.tags?.forEach((tag) => {
+      if (tagCount[tag.slug]) {
+        tagCount[tag.slug].count += 1;
+      } else {
+        tagCount[tag.slug] = { text: tag.text, count: 1 };
+      }
+    });
+  });
+
+  return Object.entries(tagCount)
+    .sort((a, b) => b[1].count - a[1].count)
+    .slice(0, 10)
+    .map(([slug, { text }]) => ({ text, slug }));
+};
