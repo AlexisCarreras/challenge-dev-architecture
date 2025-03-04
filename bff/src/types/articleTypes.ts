2b1c600a0c4404805545373fd060eb8f93169ee0
Alexis Carreras <alexiscarreras@hotmail.com>
2025-03-03 21:56:04 -0300

    feat: add types for articles and filters

---
+++ bff/src/types/articleTypes.ts
@@ -0,0 +1,54 @@
+interface Tag {
+  slug: string;
+  text: string;
+}
+
+interface Taxonomy {
+  tags?: Tag[];
+}
+
+export interface Article {
+  _id: string;
+  display_date: string;
+  headlines: {
+    basic: string;
+  };
+  promo_items?: {
+    basic?: {
+      resized_urls?: {
+        option: { media: string };
+        resizedUrl: string;
+      }[];
+      subtitle?: string;
+      type: string;
+      url: string;
+    };
+  };
+  subtype: string;
+  taxonomy?: Taxonomy;
+  website_url: string;
+}
+
+export interface FetchArticlesResponse {
+  articles: Article[];
+}
+
+export interface TopTag {
+  text: string;
+  slug: string;
+}
+
+export interface TransformedArticle {
+  id: string;
+  headline: string;
+  displayDate: string;
+  imageUrl?: string;
+  subtitle?: string;
+  tags: string[];
+  websiteUrl: string;
+}
+
+export interface TransformedArticleResponse {
+  articles: TransformedArticle[];
+  topTags: TopTag[];
+}
